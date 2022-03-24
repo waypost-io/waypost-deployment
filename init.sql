c4e2aa991f573e99e4bfd13ddb8ed904a99832e7
Caleb Smith <smithcaleb042@gmail.com>
2022-03-24 09:05:51 -0600

    updated db reset file

--- init.sql
+++ init.sql
@@ -10,6 +10,14 @@ CREATE TABLE IF NOT EXISTS flags (
     is_deleted boolean NOT NULL DEFAULT FALSE,
     date_created timestamp with time zone NOT NULL DEFAULT current_timestamp
 );
+       
+CREATE TABLE IF NOT EXISTS custom_assignments (
+    id SERIAL PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    flag_id INTEGER REFERENCES flags (id) NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE,
+    UNIQUE (user_id, flag_id)
+);
 
 CREATE TABLE IF NOT EXISTS flag_events (
     id SERIAL PRIMARY KEY,
@@ -28,6 +36,10 @@ CREATE TABLE IF NOT EXISTS connection (
     expt_table_query VARCHAR(255)
 );
 
+CREATE TABLE IF NOT EXISTS keys (
+    sdk_key VARCHAR(37) PRIMARY KEY UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS experiments (
     id SERIAL PRIMARY KEY,
     flag_id INTEGER REFERENCES flags (id),
@@ -39,32 +51,28 @@ CREATE TABLE IF NOT EXISTS experiments (
 );
 
 CREATE TABLE IF NOT EXISTS exposures (
-  id SERIAL PRIMARY KEY,
-  experiment_id INTEGER REFERENCES experiments (id) NOT NULL,
-  variant VARCHAR(7) NOT NULL,
-  num_users INT NOT NULL,
-  date DATE NOT NULL
+    id SERIAL PRIMARY KEY,
+    experiment_id INTEGER REFERENCES experiments (id) NOT NULL,
+    variant VARCHAR(7) NOT NULL,
+    num_users INT NOT NULL,
+    date DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS metrics (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(50) NOT NULL UNIQUE,
-  query_string TEXT NOT NULL,
-  type VARCHAR(10) NOT NULL,
-  is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    query_string TEXT NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS experiment_metrics (
-  experiment_id INTEGER REFERENCES experiments (id) NOT NULL,
-  metric_id INTEGER REFERENCES metrics (id) NOT NULL,
-  mean_test float,
-  mean_control float,
-  interval_start float,
-  interval_end float,
-  p_value float,
-  PRIMARY KEY (experiment_id, metric_id)
-);
-
-CREATE TABLE keys (
-    sdk_key VARCHAR(37) PRIMARY KEY UNIQUE
+    experiment_id INTEGER REFERENCES experiments (id) NOT NULL,
+    metric_id INTEGER REFERENCES metrics (id) NOT NULL,
+    mean_test float,
+    mean_control float,
+    interval_start float,
+    interval_end float,
+    p_value float,
+    PRIMARY KEY (experiment_id, metric_id)
 );
